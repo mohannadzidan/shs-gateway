3edc2453f7eb43e079102e13587839686fe23bc0
mohannadzidan <moh.anad23823@gmail.com>
2021-09-21 06:06:18 +0100

    ignoring dist directory

--- dist/dnssd-advertise.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const dnssd = require('dnssd');
-const ad = new dnssd.Advertisement(dnssd.tcp('shs'), parseInt(process.env.PORT));
-ad.start();
-//# sourceMappingURL=dnssd-advertise.js.map
